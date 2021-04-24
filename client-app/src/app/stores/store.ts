2a0518ce4cc0772d0d9c8748cd9dfd6f8a5d5993
Jose David Sanchez <jsanlopez93@gmail.com>
2021-04-24 18:49:00 -0500

    End of Section 10

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,12 +1,15 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
+import CommmonStore from "./commonStore";
 
 interface Store {
-    activityStore: ActivityStore
+    activityStore: ActivityStore;
+    commonStore: CommmonStore;
 }
 
 export const store: Store = {
-    activityStore: new ActivityStore()
+    activityStore: new ActivityStore(),
+    commonStore: new CommmonStore()
 }
 
 export const StoreContext = createContext(store);
